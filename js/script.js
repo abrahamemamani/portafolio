1986c6d934849616476eae4c089cff9acf79b6d0
Abraham Mamani <abraham.e.mamani@gmail.com>
2019-02-20 17:17:25 -0300

    [Nuevo diseño de cards][Inserción de checks en el banner][Cambio de año en el pie de página]

--- js/script.js
+++ js/script.js
@@ -4,18 +4,19 @@ new Vue({
         site: {
             title: 'Mi portafolio',
             message: '<b>*Aclaración: </b> Son solo proyectos personales (de aprendizaje), ninguno es laboral.',
-            footer: '2018 Mi portafolio. Todos los derechos reservados | Diseñado por Abraham Mamani.',
+            footer: '2019 Mi portafolio. Todos los derechos reservados | Diseñado por Abraham Mamani.',
             checks: [
-                'Desarrollador Front-end', 
-                'HTML, CSS, JavaScript, jQuery, Bootstrap', 
-                'Diseño web responsive', 
-                'Programación orientada a objetos'
+                'Desarrollador Front-End', 
+                'HTML, CSS, Bootstrap, JavaScript (jQuery, Vue.js)',
+                'PHP (Laravel), MySQL, Git/GitHub', 
+                'Diseño web responsive',
+                'Programación orientada a objetos y componentes'
             ]
         },
         cards: {
             experiences: [
                 {
-                    title: 'Clasificados - El tribuno (Salta)',
+                    title: '# Clasificados - El tribuno (Salta)',
                     image: 'images/exp/1.jpg',
                     description: 'Es un sistema de anuncios en diarios, tanto digital como en papel.',
                     date: 'Feb - Jul (2018)',
@@ -24,47 +25,47 @@ new Vue({
             ],
             applications: [
                 {
-                    title: 'Instalar',
+                    title: '# Instalar',
                     image: 'images/app/1.jpg',
                     description: 'Se trata de un mini-instagram utilizando el framework Laravel.',
-                    date: 'Nov - 2018',
+                    date: 'Nov/2018',
                     link: 'http://instalar.herokuapp.com/',
                 },
                 {
-                    title: 'Vuegames',
+                    title: '# Vuegames',
                     image: 'images/app/2.jpg',
                     description: 'Es un CRUD utilizando los frameworks Laravel y VueJS.',
-                    date: 'Oct - 2018',
+                    date: 'Oct/2018',
                     link: 'http://vuegames.herokuapp.com/',
                 }
             ],
             templates: [
                 {
-                    title: 'CoursesIT',
+                    title: '# CoursesIT',
                     image: 'images/tmp/4.jpg',
                     description: 'CoursesIT es un template para centros de capacitación tecnológicos.',
-                    date: 'Dic - 2018',
+                    date: 'Dic/2018',
                     link: 'https://abrahamemamani.github.io/Coursesit/',
                 },
                 {
-                    title: 'Foody',
+                    title: '# Foody',
                     image: 'images/tmp/1.jpg',
                     description: 'Profesional y elegante. Foody es un template para restaurantes.',
-                    date: 'Ago - 2018',
+                    date: 'Ago/2018',
                     link: 'https://abrahamemamani.github.io/foody/',
                 },
                 {
-                    title: 'Codgames',
+                    title: '# Codgames',
                     image: 'images/tmp/2.jpg',
                     description: 'Llamativo y adictivo, desde luego. Se trata de un template para un negocio de videojuegos.',
-                    date: 'Dic - 2017',
+                    date: 'Dic/2017',
                     link: 'https://abrahamemamani.github.io/codgames/',
                 },
                 {
-                    title: 'Bienstar',
+                    title: '# Bienstar',
                     image: 'images/tmp/3.jpg',
                     description: 'Buscando cumplir el sueño de todas, Bienstar está hecho para vestir bien y a la moda.',
-                    date: 'Dic - 2017',
+                    date: 'Dic/2017',
                     link: 'https://abrahamemamani.github.io/bienstar/',
                 }
             ]
